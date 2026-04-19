f2fb0179bc8c1f8fc8bf4f8ced06daa57d706a9b
danghoangphuong <helloanhchua@gmail.com>
2026-04-19 23:51:34 +0700

    vẽ xong nguồn ac và 5v

--- ESP32_smart_oulet/src/main.cpp
+++ ESP32_smart_oulet/src/main.cpp
@@ -1,10 +1,28 @@
 #include <Arduino.h>
 #include <Button_handle.h>
 #include "wifi_config.h"
+#include <BlynkSimpleEsp32.h>
+#define BLYNK_TEMPLATE_ID "TMPL6XC10hGT2"
+#define BLYNK_TEMPLATE_NAME "Home Outlet"
+#define BLYNK_AUTH_TOKEN "oYUSrMOl7aWy_7B9HzfzvTCVhcO6piQo"
+
+#define SLOT_1_PIN 32
+#define SLOT_2_PIN 33
+#define SLOT_3_PIN 34
+#define RELAY_SLOT_1 V1
+#define RELAY_SLOT_2 V2
+#define RELAY_SLOT_3 V3
 
 Button_TypeDef button_rst;
 Wifi_config wifi_conf(80); // initialize
 Wifi_config_TypeDef wifi_info;
+
+uint8_t is_blynk_connected = 0;
+unsigned long last_blynk_connect_attempt = 0;
+
+BLYNK_CONNECTED() {
+  Blynk.syncVirtual(RELAY_SLOT_1,RELAY_SLOT_2,RELAY_SLOT_3);
+}
 
 typedef enum
 {
@@ -14,10 +32,30 @@ typedef enum
 }WIFI_STATUS;
 WIFI_STATUS wifi_sta;
 
+BLYNK_WRITE(RELAY_SLOT_1){
+  int p = param.asInt();
+  digitalWrite(SLOT_1_PIN, p);
+}
+
+BLYNK_WRITE(RELAY_SLOT_2){
+  int p = param.asInt();
+  digitalWrite(SLOT_2_PIN, p);
+}
+
+BLYNK_WRITE(RELAY_SLOT_3){
+  int p = param.asInt();
+  digitalWrite(SLOT_3_PIN, p);
+}
+
 void setup() 
 {
   Serial.begin(115200);
-  Button_init(&button_rst, INPUT_PULLUP, 34);// nút rst wifi
+  pinMode(SLOT_1_PIN, OUTPUT);
+  pinMode(SLOT_2_PIN, OUTPUT);
+  pinMode(SLOT_3_PIN, OUTPUT);
+  Blynk.config(BLYNK_AUTH_TOKEN, "blynk-cloud.com", 80);
+
+  Button_init(&button_rst, INPUT_PULLUP, 35);// nút rst wifi
   pinMode(2, OUTPUT); // báo trạng thái wifi
   wifi_info.ssid = "Phuong Smart Outlet";
   wifi_info.password = "12345678";
@@ -71,4 +109,22 @@ void loop()
   wifi_conf.Handle_Client();
   Button_handle(&button_rst);
   Wifi_status_indicator();
+
+  if (wifi_conf.Is_connected()) 
+  {
+    if (!Blynk.connected()) 
+    {
+      // Cứ mỗi 10 giây mới thử kết nối Blynk 1 lần nếu đang mất kết nối
+      if (millis() - last_blynk_connect_attempt > 10000) 
+      {
+        last_blynk_connect_attempt = millis();
+        Serial.println("Connecting to Blynk...");
+        Blynk.connect();
+      }
+    } 
+    else 
+    {
+      Blynk.run();
+    }
+  }
 }
